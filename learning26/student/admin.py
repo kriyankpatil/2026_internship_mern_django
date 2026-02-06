dc5319f4ff73a1964007b2449afa5c0971410a8c
Kriyank Patil <kriyankpatil0@gmail.com>
2026-02-06 12:13:37 +0530

    feat: Introduce StudentProfile, Category, and Service models with their respective migrations and admin registrations.

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student,Product   
+from .models import Student,Product,StudentProfile,Category,Service
 
 admin.site.register(Student)
 admin.site.register(Product)
+admin.site.register(StudentProfile)
+admin.site.register(Category)
+admin.site.register(Service)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -10,6 +10,10 @@ class Student(models.Model):
     class Meta:
         db_table = "student" 
 
+    def __str__(self):
+        return self.studentName
+
+
 class Product(models.Model):
     productName = models.CharField(max_length=100)
     productPrice = models.IntegerField()
@@ -19,3 +23,47 @@ class Product(models.Model):
     productStatus = models.BooleanField(default=True)
     class Meta:
         db_table = "product"
+
+
+class StudentProfile(models.Model):
+    hobbies =(("reading","reading"),("travel","travel"),("music","music"))
+    #studentPrilfe id --> pk create auto...
+    studentId = models.OneToOneField(Student,on_delete=models.CASCADE)
+    studentHobbies = models.CharField(max_length=100,choices=hobbies)
+    studentAddress = models.CharField(max_length=100)
+    studentPhone = models.CharField(max_length=10)
+    studentGender = models.CharField(max_length=10)
+    studentDOB = models.DateField()
+    
+    class Meta:
+        db_table = "studentprofile"
+
+    def __str__(self):
+        return self.studentId.studentName
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=100)
+    categoryDescription = models.TextField()
+    categoryStatus = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "category"
+
+    def __str__(self):
+        return self.categoryName    
+
+class Service(models.Model):
+    serviceName = models.CharField(max_length=100)
+    serviceDescription = models.TextField()
+    servicePrice = models.IntegerField()
+    serviceStatus = models.BooleanField(default=True)
+    #after table creation adding new field
+    discount = models.IntegerField(null=True)
+    categoryId = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    
+    class Meta:
+        db_table = "service"
+
+    def __str__(self):
+        return self.serviceName    
